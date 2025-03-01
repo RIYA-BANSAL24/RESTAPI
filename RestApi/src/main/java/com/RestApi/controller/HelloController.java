18aad28ab1c1af8d2cb66a712a938f631b1b4cb6
RIYA_BANSAL24 <riyabansal3365@gmail.com>
2025-03-01 23:06:07 +0530

    Name send from query

--- RestApi/src/main/java/com/RestApi/controller/HelloController.java
+++ RestApi/src/main/java/com/RestApi/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.RestApi.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/hello")
@@ -9,5 +11,13 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         return "Hello from Riya!!";
+    }
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from Riya";
+    }
+    @GetMapping("/param/{name}")
+    public String sayHelloWithPathVar(@PathVariable String name) {
+        return "Hello " + name + " from Riya";
     }
 }
